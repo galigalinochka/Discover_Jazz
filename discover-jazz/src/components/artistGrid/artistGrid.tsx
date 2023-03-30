1bc86c0cd465c2bf59f0ea6a155160e9e22ddabf
galina_ka <galina_riabkova@mail.ru>
2023-03-30 19:18:05 +0300

    feat: add tags to App.tsx

---
+++ discover-jazz/src/components/artistGrid/artistGrid.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './artistGrid.module.css';
+
+const artistGrid = () => {
+  return (
+    <div>
+      Artists
+    </div>
+  );
+};
+
+export default artistGrid;
---
+++ discover-jazz/src/components/mainCard/mainCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './mainCard.module.css';
+
+const Main = () => {
+  return (
+    <div>
+      MainBlock
+    </div>
+  );
+};
+
+export default Main;
