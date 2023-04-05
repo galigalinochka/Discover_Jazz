9a51c0f40cc57f762cbbac9bcbb8c19acb510078
galina_ka <galina_riabkova@mail.ru>
2023-04-05 13:13:24 +0300

    feat: add type button and styles

---
+++ discover-jazz/src/components/ui/textDiv/textDiv.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import styles from '../ui.module.css';
+import { DivElement } from '../../../types/types';
+
+const DivElement: FC = () => {
+  return (
+  <div className = {styles.container}>
+    <div></div>
+    </div>
+  )
+}
+
+export default DivElement;
