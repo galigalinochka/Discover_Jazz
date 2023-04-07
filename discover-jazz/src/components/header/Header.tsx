3754c06cbd46f8ee26b1c898e50cac9da9b9df86
NickTolno <laidebonny@gmail.com>
2023-04-07 20:06:08 +0700

    solve problemsd with develop

--- discover-jazz/src/components/header/Header.tsx
+++ discover-jazz/src/components/header/Header.tsx
@@ -14,6 +14,12 @@ type Props ={};
 
 export const Header = (props: Props) => {
   const isDesktop = useMediaQuery({
+    query: '(min-width: 1411px)'
+  });
+  const isDesktopMini = useMediaQuery({
+    query: '(min-width: 980px)'
+  });
+  const isLapTop = useMediaQuery({
     query: '(min-width: 769px)'
   });
 
@@ -21,8 +27,12 @@ export const Header = (props: Props) => {
 
   return (
     
-    <StyledHeader>
-      {isDesktop ? (
+
+      
+
+    <div className={styles.headerStyle}>
+      {isDesktop && (
+
         <>
           <div className={styles.headerElementBlockDesktop}>
             <HeaderElement heading={'tickets'} />
@@ -49,7 +59,52 @@ export const Header = (props: Props) => {
             <HeaderElement heading={'on sale now'} />
           </div>
         </>
-      ) : (
+      )}
+      {isDesktopMini && !isDesktop &&(
+        <>
+        <div className={styles.headerElementBlockDesktop}>
+        <HeaderElement heading={'tickets'} />
+        <img src={ArrowIcon} alt="Arrow Icon" />
+      </div>
+      <div className={styles.headerElementBlockDesktop}>
+        <img src={PointIcon} alt="Point Icon" />
+        <HeaderElement heading={'on sale now'} />
+      </div>
+      <div className={styles.headerElementBlockDesktop}>
+        <HeaderElement heading={'tickets'} />
+        <img src={ArrowIcon} alt="Arrow Icon" />
+      </div>
+      <div className={styles.headerElementBlockDesktop}>
+        <img src={PointIcon} alt="Point Icon" />
+        <HeaderElement heading={'on sale now'} />
+      </div>
+      </>
+      )}
+      {isLapTop && !isDesktop && !isDesktopMini && (
+        <>   
+        <div className={styles.headerElementBlockLapTop}>
+          <img src={PointIcon} alt="Point Icon" />
+          <HeaderElement heading={'on sale now'} />
+        </div>       
+        <div className={styles.headerElementBlockLapTop}>
+          <HeaderElement heading={'tickets'} />
+          <img src={ArrowIcon} alt="Arrow Icon" />
+        </div>
+        <div className={styles.headerElementBlockLapTop}>
+          <img src={PointIcon} alt="Point Icon" />
+          <HeaderElement heading={'on sale now'} />
+        </div>
+        <div className={styles.headerElementBlockLapTop}>
+          <HeaderElement heading={'tickets'} />
+          <img src={ArrowIcon} alt="Arrow Icon" />
+        </div>
+        <div className={styles.headerElementBlockLapTop}>
+          <img src={PointIcon} alt="Point Icon" />
+          <HeaderElement heading={'on sale now'} />
+        </div>
+      </>
+      )}
+      {!isLapTop && !isDesktop && !isDesktopMini &&(
         <>
           <div className={styles.headerElementBlockMobile}>
             <HeaderElement heading={'tickets'} />
@@ -65,12 +120,6 @@ export const Header = (props: Props) => {
           </div>
         </>
       )}
-    </StyledHeader>
+    </div>
   );
 };
-
-
-
-
-
-
