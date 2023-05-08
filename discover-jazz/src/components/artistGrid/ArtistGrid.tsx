29463df93873f006bdafdb9b4d6633f8731292e5
NickTolno <laidebonny@gmail.com>
2023-05-08 14:50:52 +0700

    changes

--- discover-jazz/src/components/artistGrid/ArtistGrid.tsx
+++ discover-jazz/src/components/artistGrid/ArtistGrid.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import { useState, createContext, useContext } from "react";
 import styles from './artistGrid.module.css';
 import { StyledArtistGrid } from '../artistGrid/artistGrid.styles'
 import ArtistElement from '../artistGrid/ArtistElement'
 import data from '../../data/artists';
 import { IArtist } from "../../../src/types/types"
-import SelectBlockProps from '../mainCard/selectBlock'
-import filteredData from '../mainCard/selectBlock'
+import {FilteredDataResult} from '../../components/mainCard/selectBlock'
 
-
-
-
-console.log(filteredData)
 
 const Artists = () => {
+
+  const filteredData = useContext(FilteredDataResult);
+
+  // console.log(filteredData);
+  
   return (
     <StyledArtistGrid>
-      {data.map(artist => {
+      {filteredData.map((artist: IArtist) => {
         // console.log(artist.id);
         // console.log(artist.photo);
         return (
@@ -33,6 +33,34 @@ const Artists = () => {
 };
 
 export default Artists;
+
+
+
+
+// const Artists = () => {
+
+//   const filteredData = useContext(FilteredDataResult);
+
+//   return (
+//     <StyledArtistGrid>
+//       {filteredData.map(artist => {
+//         // console.log(artist.id);
+//         // console.log(artist.photo);
+//         return (
+//           <ArtistElement          
+//             name={artist.name}
+//             big={artist.name === 'Gregory Porter' || artist.name === 'Esperanza Spalding'}
+//             photo={artist.photo}
+//             id={artist.id}
+//           />
+//         );
+//       })}
+//     </StyledArtistGrid>
+    
+//   );  
+// };
+
+// export default Artists;
  
 
 
